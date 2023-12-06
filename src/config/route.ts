d98d34f12f5b3eec46aa8448b6fd6d1d82db3e91
1968210376 <65202465+1968210376@users.noreply.github.com>
2023-12-06 20:55:21 +0800

    Update route.ts

--- src/config/route.ts
+++ src/config/route.ts
@@ -1,39 +1,17 @@
-import IndexVue from "../pages/Index.vue";
-import TeamVue from "../pages/Team.vue";
-import UserUpdate from "../pages/UserUpdate.vue";
-import Search from "../pages/Search.vue";
-import EditUser from "../pages/EditUser.vue";
-import EditTag from "../pages/addTag.vue";
-import SearchResultVue from "../pages/SearchResult.vue";
-import UserLoginPage from "../pages/UserLogin.vue";
-import UserRegisterVue from "../pages/UserRegister.vue";
-import TeamAddPageVue from "../pages/TeamAddPage.vue";
-import TeamUpdatePageVue from "../pages/TeamUpdatePage.vue";
-import UserTeamCreate from "../pages/UserTeamCreate.vue";
-import UserTeamJoin from "../pages/UserTeamJoin.vue";
-
 // 定义路由
 const routes = [
-  { path: "/", component: IndexVue },
-  { path: "/search", title: "搜索队友", component: Search },
-  { path: "/team", title: "队伍", component: TeamVue },
-  { path: "/team/add", title: "创建队伍", component: TeamAddPageVue },
-  { path: "/team/update", title: "编辑队伍", component: TeamUpdatePageVue },
-  { path: "/user/login", title: "登录", component: UserLoginPage },
-  { path: "/user/register", title: "注册", component: UserRegisterVue },
-  { path: "/userinfo/update", title: "用户信息", component: UserUpdate },
-  { path: "/userinfo/edit", title: "编辑信息", component: EditUser },
-  { path: "/userinfo/edittag", title: "编辑标签", component: EditTag },
-  { path: "/userinfo/list", title: "搜索结果", component: SearchResultVue },
-  {
-    path: "/userinfo/team/create",
-    title: "我创建的队伍",
-    component: UserTeamCreate,
-  },
-  {
-    path: "/userinfo/team/join",
-    title: "我加入队伍",
-    component: UserTeamJoin,
-  },
+  { path: "/", component: () => import("../pages/Index.vue") },
+  { path: "/search", title: "搜索队友", component: () => import("../pages/Search.vue")},
+  { path: "/team", title: "队伍", component: () => import("../pages/Team.vue")},
+  { path: "/team/add", title: "创建队伍", component: () => import("../pages/TeamAddPage.vue")},
+  { path: "/team/update", title: "编辑队伍", component: () => import("../pages/TeamUpdatePage.vue") },
+  { path: "/user/login", title: "登录", component: () => import("../pages/UserLogin.vue") },
+  { path: "/user/register", title: "注册", component: () => import("../pages/UserRegister.vue") },
+  { path: "/userinfo/update", title: "用户信息", component: () => import("../pages/UserUpdate.vue") },
+  { path: "/userinfo/edit", title: "编辑信息", component: () => import("../pages/EditUser.vue") },
+  { path: "/userinfo/edittag", title: "编辑标签", component: () => import("../pages/addTag.vue") },
+  { path: "/userinfo/list", title: "搜索结果", component: () => import("../pages/SearchResult.vue") },
+  { path: "/userinfo/team/create", title: "我创建的队伍", component: () => import("../pages/UserTeamCreate.vue") },
+  { path: "/userinfo/team/join", title: "我加入队伍", component: () => import("../pages/UserTeamJoin.vue") },
 ];
 export default routes;
